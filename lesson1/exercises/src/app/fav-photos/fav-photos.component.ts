823635f5c429aa983346732be9355fc5bb7a8aac
Jane Byrne <janebyrne92@gmail.com>
2019-12-11 23:59:46 -0600

    lesson 1 part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://boatsibiza.com/wp-content/uploads/2017/07/seahorses.jpg';
+  image3 = 'http://pm1.narvii.com/6777/73f5fbd1ad12a41df661f783180e9282aed1f5d3v2_00.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'Nightmare Before Christmas', 'The Road to El Dorado', 'Eternal Sunshine of the Spotless Mind'];
 
    constructor() { }
 
